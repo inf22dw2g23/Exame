ea09cc38d2a027a6cb72296d2c70068314c31930
João Aragão <81379038+JoaoAragaoA039132@users.noreply.github.com>
2023-06-28 01:42:54 +0100

    .

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:14
-ENV NODE_ENV=production
-WORKDIR /M1
-COPY . .
-RUN npm install --production -silent
-RUN chown -R node /M1
-EXPOSE 3000
-USER node
-CMD ["npm", "start"]
--- plataforma_jogos_database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Partidas (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(255) NOT NULL,
-  jogador_id INT,
-  FOREIGN KEY (jogador_id) REFERENCES Jogadores(id)
-);
